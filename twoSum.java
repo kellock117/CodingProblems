139d931901d85f8350031ef8ddde292e1f377dde
kellock117 <92283523+kellock117@users.noreply.github.com>
2022-08-17 14:05:04 +0800

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -1,44 +1,21 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-class twoSum {
-    public static int[] ts(int[] nums, int target) {
-        int [] a;
-        int k;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         
-        for(int i=0; i<nums.length; i++){
-            k = 0;
-            k += nums[i];
-            for(int j=0; j<nums.length; j++){
-                if(i==j)
-                    continue;
-                else
-                    k += nums[j];
-                
-                if(k==target){
-                    return new int[] {i,j};
-                }
-                k -= nums[j];
+        for(int i = 0; i < nums.length; i++) {
+            int gap = target - nums[i];
+            if(map.containsKey(gap)) {
+                return new int[] {i, map.get(gap)};
             }
+            else {
+                map.put(nums[i], i);
+            } 
         }
-        return new int[] {-1,-1};
-    }
-    
-    public static int[] tS(int[] numbers, int target) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < numbers.length; map.put(numbers[i], i++)) {
-        	System.out.println(map);
-            if (map.containsKey(target - numbers[i])) 
-                return new int[]{map.get(target - numbers[i]),i};
-        }
-        return new int[]{0,0};
-    }
-    
-    public static void main(String[]args) {
-    	int [] a = {3,2,4};
-    	System.out.println();
-    	System.out.println(Arrays.toString(tS(a,6)));
-    	System.out.println(Arrays.toString(ts(a,6)));
+        
+        return new int[0]; 
     }
 }
+
+// https://leetcode.com/problems/two-sum/submissions/
+// Runtime: 1 ms, faster than 99.76% of Java online submissions for Two Sum.
+// Memory Usage: 42.2 MB, less than 98.51% of Java online submissions for Two Sum.
